82fb95c6f0ad02ec14a0fdda764c31e1bcd059b7
zollahungry <zollahungry@naver.com>
2018-06-16 18:14:27 +0900

    GameManager생성
    더미 땅 6개로 늘림
    const int 변수로  땅 개수 필요한 곳에 사용
    
    null 되는 시간 때문에 땅 이동속도가 달라지면 current땅과 next 땅 사이의 빈 틈이 생기던 것 해결.
    이미지가 이미지 끝에 안 붙는 문제 생김

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance = null;
+
+    private GameManager()
+    {
+    }
+    public static GameManager Instance
+    {
+        get
+        {
+            if(instance == null)
+                instance = new GameManager();
+            return instance;
+        }
+    }
+
+    public float groundSpeed;
+}
--- Assets/Script/GroundManager.cs
+++ Assets/Script/GroundManager.cs
@@ -2,29 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundManager : MonoBehaviour {
-    
-    Transform[] grounds = new Transform[3]; // 땅 스프라이트 개수
-    Transform currentGround;
-    Transform nextGround;
-    SpriteRenderer currentSprite;
-    SpriteRenderer nextSprite;
+public class GroundManager : MonoBehaviour
+{
+
+    const int GROUNDSPRITE = 6;
+
+    Transform[] _grounds = new Transform[GROUNDSPRITE]; // 땅 스프라이트 개수
+    Transform _currentGround;
+    Transform _nextGround;
+    SpriteRenderer _currentSprite;
+    SpriteRenderer _nextSprite;
 
     public float groundSpeed;
 
     void Init()
     {
-        // 자식인 땅 이미지 Transform 배열에 넣음
-        for (int i = 0; i < transform.childCount; i++)
+        groundSpeed = 0.1f;
+
+        // GroundManager오브젝트의 자식인 스프라이트 가진 오브젝트들을 Transform 배열에 넣음
+        for (int i = 0; i < GROUNDSPRITE; i++)
         {
-            grounds[i] = transform.Find("DummyGround" + i.ToString());
+            _grounds[i] = transform.Find("DummyGround" + i.ToString());
         }
 
-        // current는 grounds 배열요소 중 하나
-        currentGround = grounds[Random.Range(0, 3)];
-        currentGround.gameObject.SetActive(true);
+        // current와 next에 각각 grounds[0],grounds[1] 넣고 시작
+        _currentGround = _grounds[0];
+        _nextGround = _grounds[1];
 
-        groundSpeed = 0.1f;
+        _currentSprite = _currentGround.GetComponent<SpriteRenderer>();
+        _nextSprite = _nextGround.GetComponent<SpriteRenderer>();
+
+        // current 오른쪽 끝에 next 위치시키고 켬
+        _nextGround.transform.position = _currentSprite.bounds.size * Vector2.right;
+
+
+        _currentGround.gameObject.SetActive(true);
+        _nextGround.gameObject.SetActive(true);
     }
 
     void Awake()
@@ -39,37 +52,43 @@ public class GroundManager : MonoBehaviour {
 
     void MovingGround()
     {
-        // 땅 이동, next가 null이 아니면 같이 움직임
-        if (currentGround != null)
-            currentGround.transform.Translate(new Vector2(-groundSpeed, 0));
-        if (nextGround != null)
-            nextGround.transform.Translate(new Vector2(-groundSpeed, 0));
-        
-        if (currentGround.transform.position.x <= 0 && nextGround == null) // current가 중간에 오고, next가 비어있으면
+        // 땅 이동, 양쪽이 null이 아니면 움직임
+        if (_currentGround != null && _nextGround != null)
         {
-            do
-            {
-                nextGround = grounds[Random.Range(0, 3)];
-            } while (nextGround == currentGround);
-
-            currentSprite = currentGround.GetComponent<SpriteRenderer>();
-            nextSprite = nextGround.GetComponent<SpriteRenderer>();
-
-            nextGround.transform.position = currentSprite.bounds.size * Vector2.right;
-            nextGround.gameObject.SetActive(true);
+            _currentGround.transform.Translate(new Vector2(-groundSpeed, 0));
+            _nextGround.transform.Translate(new Vector2(-groundSpeed, 0));
         }
 
-        if (currentSprite != null)
-        { 
+        // next가 null이 되면 next에 ground 집어넣는다
+        else
+        {
+            do
+            { // 랜덤으로 ground[]에서 넣고 current와 같다면 반복
+                _nextGround = _grounds[Random.Range(0, GROUNDSPRITE)];
+            } while (_nextGround == _currentGround);
+
+            // next가 null 아니고, next가 current와 같지 않으면 이어붙이고 켬
+            if (_nextGround != null && _nextGround != _currentGround)
+            {
+                _currentSprite = _currentGround.GetComponent<SpriteRenderer>();
+                _nextSprite = _nextGround.GetComponent<SpriteRenderer>();               
+                
+                // 이어붙이고 켬
+                _nextGround.transform.position = _currentSprite.bounds.size * Vector2.right;
+                _nextGround.gameObject.SetActive(true);
+            }
+        }
+        
+        if (_currentSprite != null)
+        {
             // current가 화면 밖일 때
-            if (currentSprite.gameObject.activeInHierarchy 
-                && nextSprite != null 
-                && currentSprite.bounds.max.x <= Camera.main.ScreenToWorldPoint(Vector2.zero).x)
+            if (_currentSprite.gameObject.activeInHierarchy
+                && _currentSprite.bounds.max.x <= Camera.main.ScreenToWorldPoint(Vector2.zero).x)
             {
                 // current 끄고, current에 next 넣고, next는 null
-                currentGround.gameObject.SetActive(false);
-                currentGround = nextGround;
-                nextGround = null;
+                _currentGround.gameObject.SetActive(false);
+                _currentGround = _nextGround;
+                _nextGround = null;
             }
         }
     }
